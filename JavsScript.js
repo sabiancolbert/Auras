24458102e087b4c07fae9bf5dd5d55c39650019c
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2020-12-14 21:58:53 -0600

    5

--- JavsScript.js
+++ JavsScript.js
@@ -1,5 +1,24 @@
 function $($) { return document.getElementById($); }
 
+function onLoad() {
+    if (window.innerWidth < 705) {
+        $("info").style.top = "880px";
+    }
+}
+
+function onResize() {
+    if (window.innerWidth < 705) {
+        $("info").style.margin = "0 auto";
+        $("info").style.top = "880px";//here
+        //here redo body image, ddivs, and chakras
+        //redraw info to center it (or resize width while preventing onResize to run (to avoid a loop))
+        $("info").style.display = "none";
+        $("info").offsetHeight;
+        $("info").style.display = "block";
+        window.onResize();
+    }
+}
+
 function loc(x) {
     $(x).style.backgroundColor = x;
     $(x).style.width = "150px";
