ce0c27a60bfe5a800685089badacd429775f5892
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-02-02 10:15:22 -0600

    s

--- JavsScript.js
+++ JavsScript.js
@@ -1,7 +1,19 @@
+class Chakra {
+    constructor(name, found, frequency, sense, good, bad, description) {
+        this.name = name;
+        this.found = found;
+        this.frequency = frequency;
+        this.sense = sense;
+        this.good = good;
+        this.bad = bad;
+        this.description = description;
+    }
+}
+
 function $($) { return document.getElementById($); }
 var runnning = false;
+var chakras = [new Chakra("Muladhara: Root Chakra", "Level with tailbone", "396-432<br>Hz", "Smell", "Safe<br>Calm<br>Secure", "Fear<br>Anxiety<br>Guilt", "Connection between you and your body, physical pleasure ,is the foundation for the other chakras, high good physical health, kow is frustration")];
 window.onload = function () { adjust(); }
-
 window.onresize = function () { adjust(); }
 
 function adjust() {
@@ -35,4 +47,14 @@ function loc(x) {
 function clo(x) {
     $(x).style.backgroundColor = "transparent";
     $(x).style.width = "57px";
+}
+
+function setChakra(x) {
+    $("header").innerHTML = chakras[x].name;
+    $("found").innerHTML = chakras[x].found;
+    $("good").innerHTML = chakras[x].good;
+    $("bad").innerHTML = chakras[x].bad;
+    $("frequency").innerHTML = chakras[x].frequency;
+    $("sense").innerHTML = chakras[x].sense;
+    $("description").innerHTML = chakras[x].description;
 }
